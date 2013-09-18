c70698ec47b5accbb767074cdf23bea564c38af5
erdie <erdie.one@gmail.com>
2013-09-18 11:01:29 +0700

    Create launcher for pahe-smart

---
+++ launcher.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+#!/bin/bash
+# Shell script sederhana untuk instalasi dan men-dial modem smartfren
+# Diuji pada Ubuntu 13.04 Raring Ringtail 32 bit
+# ----------------------------------------------------------------------------
+# Ditulis oleh Anna Erdiawan <http://erdinote.com/>
+# (c) 2013 erdinote.com under CC-BY-SA
+# <http://creativecommons.org/licenses/by-sa/3.0/>
+# ----------------------------------------------------------------------------
+# Terakhir diupdate: 8/Juni/2013
+# ----------------------------------------------------------------------------
+[[ $(id -u) -ne 0 ]] && { echo "$0: Anda harus masuk sebagai Root untuk menjalankan script ini, jalankan perintah 'sudo $0'"; exit 1; }
+
+lagi='y'
+while  [ $lagi == 'y' ] || [ $lagi == 'Y' ];
+do
+   clear
+echo -n "Menu yang tersedia untuk Anda, status: "; whoami
+echo "1. Install Launcher Smartfren Dialer untuk Ubuntu 12.04, 12.10, atau 13.04"
+echo "2. Install Launcher Smartfren Dialer untuk Elementary OS Luna"
+echo "3. Hapus Launcher Smartfren Dialer untuk Ubuntu 12.04, 12.10, atau 13.04"
+echo "4. Hapus Launcher Smartfren Dialer untuk Elementary OS Luna"
+echo "5. Keluar"
+read -p "Pilihan Anda [1-5] :" pil;
+
+if [ $pil -eq 1 ]; 
+then
+	echo "Melakukan instalasi..."
+	echo "Menyalin konfigurasi wvdial"
+	sudo cat input > /etc/wvdial.conf
+	echo "Menyalin konfigurasi selesai"
+	sleep 1
+	mkdir /opt/pahe
+	sleep 1
+	cp smart.sh /opt/pahe
+	sleep 1
+	chmod 755 /opt/pahe/smart.sh
+	sleep 1
+	cp smart.png /usr/share/icons
+	sleep 1
+	cp smart.desktop ~/Desktop
+	sleep 1
+	chmod 755 ~/Desktop/smart.desktop
+	sleep 1
+	echo "Instalasi selesai"
+	echo "Launcher terdapat pada desktop"
+elif [ $pil -eq 2 ]; 
+then
+	echo "Melakukan instalasi..."
+	echo "Menyalin konfigurasi wvdial"
+	sudo cat input > /etc/wvdial.conf
+	echo "Menyalin konfigurasi selesai"
+	sleep 1
+	mkdir /opt/pahe
+	sleep 1
+	cp smart.sh /opt/pahe
+	sleep 1
+	chmod 755 /opt/pahe/smart.sh
+	sleep 1
+	cp smart.png /usr/share/icons
+	sleep 1
+	cp smart.desktop /usr/share/applications
+	sleep 1
+	chmod 755 /usr/share/applications/smart.desktop
+	sleep 1
+	echo "Instalasi selesai"
+	echo "Silakan akses di Appplication > System > Smartfren Dialler"
+elif [ $pil -eq 3 ]; 
+then
+	echo "Uninstall..."
+	sleep 1
+	rm -R /opt/pahe
+	sleep 1
+	rm /usr/share/icons/smart.png
+	sleep 1
+	rm ~/Desktop/smart.desktop
+	sleep 1
+	echo "Uninstall selesai"
+elif [ $pil -eq 4 ]; 
+then
+	echo "Uninstall..."
+	sleep 1
+	rm -R /opt/pahe
+	sleep 1
+	rm /usr/share/icons/smart.png
+	sleep 1
+	rm /usr/share/applications/smart.desktop
+	sleep 1
+	echo "Uninstall selesai"
+elif [ $pil -eq 5 ]; 
+then
+   exit 0
+else
+   echo ""
+   echo "Maaf, menu tidak tersedia !"
+   exit 1
+fi
+echo -n "Kembali ke menu (y/t) :";
+read lagi;
+done
